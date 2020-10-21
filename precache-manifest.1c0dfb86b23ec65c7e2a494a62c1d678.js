1c1eff251cf1875e7b8abb589bddcef525970fd0
khizarkhizar <khizarrana756@gmail.com>
2020-10-21 13:51:54 +0500

    Updates

---
+++ precache-manifest.1c0dfb86b23ec65c7e2a494a62c1d678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c52dcb048c5750f9904b5946913f54",
+    "url": "/Assignment_app/index.html"
+  },
+  {
+    "revision": "b8f9728a6e8dd29ef42f",
+    "url": "/Assignment_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7069480d544217063b0",
+    "url": "/Assignment_app/static/js/2.58bd5280.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Assignment_app/static/js/2.58bd5280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f9728a6e8dd29ef42f",
+    "url": "/Assignment_app/static/js/main.635d5fba.chunk.js"
+  },
+  {
+    "revision": "9b5f84659d5026442eb6",
+    "url": "/Assignment_app/static/js/runtime-main.9934741f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Assignment_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
